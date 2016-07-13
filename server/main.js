ffba3e9bd58da8688165bacddff39217862bba4e
velopert <public.velopert@gmail.com>
2016-07-13 19:43:45 +0900

    03 모듈 설치 및 적용

--- server/main.js
+++ server/main.js
@@ -1,23 +1,46 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
+
+/* express middlewares */
+import morgan from 'morgan'; // HTTP REQUEST LOGGER
+import bodyParser from 'body-parser'; // PARSE HTML BODY
+import session from 'express-session';
 
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
+
+
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/codelab');
 
 const app = express();
 const port = 3000;
 const devPort = 4000;
 
+/* setup express middlewares */
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+/* use session */
+app.use(session({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
+
+/* serve static files */
 app.use('/', express.static(path.join(__dirname, './../public')));
 
-app.get('/hello', (req, res) => {
-    return res.send('Hello CodeLab');
-});
-
+/* listen server */
 app.listen(port, () => {
     console.log('Express is listening on port', port);
 });
 
+/* setup webpack devserver */
 if(process.env.NODE_ENV == 'development') {
     console.log('Server is running on development mode');
     const config = require('../webpack.dev.config');
