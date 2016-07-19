d1d3f34f7423e5cb0e6a5226bc71f5ccd2d0adb6
velopert <public.velopert@gmail.com>
2016-07-19 13:20:56 +0000

    19. 무한 스크롤링 구현

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,7 +7,13 @@ class Home extends React.Component {
     
     constructor(props) {
         super(props);
+        
         this.loadNewMemo = this.loadNewMemo.bind(this);
+        this.loadOldMemo = this.loadOldMemo.bind(this);
+        
+        this.state = {
+            loadingState: false
+        };
     }
     
     componentDidMount() {
@@ -17,17 +23,56 @@ class Home extends React.Component {
             this.memoLoaderTimeoutId = setTimeout(loadMemoLoop, 5000);
         };
         
+        let loadUntilScrollable = () => {
+            // IF THE SCROLLBAR DOES NOT EXIST,
+            if($("body").height() < $(window).height()) {
+                this.loadOldMemo().then(
+                    () => {
+                        // DO THIS RECURSIVELY UNLESS IT'S LAST PAGE
+                        if(!this.props.isLast) {
+                            loadUntilScrollable();
+                        }
+                    }
+                )
+            }
+        };
+        
         this.props.memoListRequest(true).then(
             () => {
                 // BEGIN NEW MEMO LOADING LOOP
+                loadUntilScrollable();
                 loadMemoLoop();
             }
         );
+        
+        
+        $(window).scroll(() => {
+            // WHEN HEIGHT UNDER SCROLLBOTTOM IS LESS THEN 250
+            if ($(document).height() - $(window).height() - $(window).scrollTop() < 250) {
+                if(!this.state.loadingState){
+                    this.loadOldMemo();
+                    this.setState({
+                        loadingState: true
+                    });
+                }
+            } else {
+                if(this.state.loadingState){
+                    this.setState({
+                        loadingState: false
+                    });
+                }
+            }
+        });
+                
+                
     }
     
-    componentWillUnMount() {
+    componentWillUnmount() {
         // STOPS THE loadMemoLoop
         clearTimeout(this.memoLoaderTimeoutId);
+        
+        // REMOVE WINDOWS SCROLL LISTENER
+        $(window).unbind();
     }
     
     loadNewMemo() {
@@ -42,6 +87,28 @@ class Home extends React.Component {
             return this.props.memoListRequest(true);
             
         return this.props.memoListRequest(false, 'new', this.props.memoData[0]._id);
+    }
+    
+    loadOldMemo() {
+        // CANCEL IF USER IS READING THE LAST PAGE
+        if(this.props.isLast) {
+            return new Promise(
+                (resolve, reject)=> {
+                    resolve();
+                }
+            );
+        }
+        
+        // GET ID OF THE MEMO AT THE BOTTOM
+        let lastId = this.props.memoData[this.props.memoData.length - 1]._id;
+        
+        // START REQUEST
+        return this.props.memoListRequest(false, 'old', lastId).then(() => {
+            // IF IT IS LAST PAGE, NOTIFY
+            if(this.props.isLast) {
+                Materialize.toast('You are reading the last page', 2000);
+            }
+        });
     }
     
     render() {
@@ -69,7 +136,8 @@ const mapStateToProps = (state) => {
         postStatus: state.memo.post,
         currentUser: state.authentification.status.currentUser,
         memoData: state.memo.list.data,
-        listStatus: state.memo.list.status
+        listStatus: state.memo.list.status,
+        isLast: state.memo.list.isLast
     };
 };
 
